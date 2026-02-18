a9c51c01553388fd56d8fb125d4dac80d0738091
Moulya15 <ksmoulya2003@gmail.com>
2026-02-18 23:01:46 +0530

    formated some files

--- src/main/java/com/example/spring_api/Controller/AdminController.java
+++ src/main/java/com/example/spring_api/Controller/AdminController.java
@@ -8,9 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
-
 @RestController
 @CrossOrigin(origins="*")
 @RequestMapping("/admin")
